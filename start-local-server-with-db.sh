ce1bffa3515cf68bdd576dce9e5262bce0356f43
Chris <chrisatkins55@gmail.com>
2025-01-05 14:46:53 -0500

    Config updates for postgres

--- start-local-server-with-db.sh
+++ start-local-server-with-db.sh
@@ -1,18 +1,16 @@
 # cleanup any old stuff
 docker network create tpm-tools-network || true
-docker stop tpm-mysql || true
+docker stop tpm-tools-db || true
 docker stop tpm-tools-backend || true
-docker rm tpm-mysql || true
+docker rm tpm-tools-db || true
 docker rm tpm-tools-backend || true
 docker rmi tpm-tools-backend-image || true
 
+docker run --restart=always --name tpm-tools-db -e POSTGRES_DB=tpm-tools -e POSTGRES_USER=local-user -e POSTGRES_PASSWORD=pwd2 --network tpm-tools-network -p 5432:5432 -d postgres:16
 
-docker run --restart=always --name tpm-mysql -e MYSQL_ROOT_PASSWORD=pwd -e MYSQL_DATABASE=tpm-tools -e MYSQL_USER=local-user -e MYSQL_PASSWORD=pwd2 --network tpm-tools-network -p 3306:3306 -d mysql:8.0.27
-
-sleep 60
-
-
+sleep 30
 
 docker build . -t tpm-tools-backend-image -f m2Dockerfile
+docker run -d --restart=on-failure:5 -p 8080:8080 -e DB_CONNECTION_STRING=jdbc:postgresql://tpm-tools-db:5432/tpm-tools -e DB_USER=local-user -e DB_PASSWORD=pwd2 --network tpm-tools-network --name tpm-tools-backend tpm-tools-backend-image
 
-docker run -d --restart=on-failure:5 -p 8080:8080 -e DB_CONNECTION_STRING=jdbc:mysql://tpm-mysql:3306/tpm-tools -e DB_USER=local-user -e DB_PASSWORD=pwd2 --network tpm-tools-network --name tpm-tools-backend tpm-tools-backend-image
+sleep 30
